481d63464b8994cdea6270326e18218bfd0284aa
BENLINB <135228073+BENLINB@users.noreply.github.com>
2024-07-12 12:16:23 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull benlin1234/simple-python-flask-app 
+sudo docker pull benlin1234/simple-python-flask-app 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 benlin1234/simple-python-flask-app 
+sudo docker run -d -p 5000:5000 benlin1234/simple-python-flask-app 
